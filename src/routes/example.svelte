742908675fc04ea9a6efc092940de53341fe0e4b
Katsuki <Katsuki_Sato0130@icloud.com>
2022-04-04 17:43:32 +0900

    Update example

--- src/routes/example.svelte
+++ src/routes/example.svelte
@@ -6,111 +6,91 @@
 	import TimelineConnector from '$lib/components/TimelineConnector.svelte';
 	import TimelineContent from '$lib/components/TimelineContent.svelte';
 	import TimelineOppositeContent from '../lib/components/TimelineOppositeContent.svelte';
+
+	const colors = {
+		first: '#7CD5E2',
+		second: '#FEC048',
+		third: '#DD84C1',
+		forth: '#FB6958'
+	};
+
+	const items = [
+		{
+			year: '1950s',
+			title: 'The first ever WAN',
+			description: 'The first known WAN was created by the U.S. Air Force.',
+			color: colors.first
+		},
+		{
+			year: '1969',
+			title: 'First online message',
+			description: 'The first online message was sent.',
+			color: colors.second
+		},
+		{
+			year: '1971',
+			title: 'First Email',
+			description: 'The first network email was sent by computer engineer Ray Tomlinson.',
+			color: colors.third
+		},
+		{
+			year: '1991',
+			title: 'First WebPage',
+			description: 'The first web page went live.',
+			color: colors.forth
+		},
+
+		{
+			year: '1995',
+			title: 'Amazon Launch',
+			description: 'Amazon was founded by Jeff Bezos from his garage.',
+			color: colors.first
+		},
+		{
+			year: '1998',
+			title: 'Google Launch',
+			description: 'Google was founded by Sergey Brin and Larry Page.',
+			color: colors.second
+		},
+		{
+			year: '2004',
+			title: 'Facebook Launch',
+			description: 'A Harvard sophomore named Mark Zuckerberg launches The Facebook.',
+			color: colors.third
+		},
+		{
+			year: '2022',
+			title: '5 billion internet users',
+			description: 'More than 5 billion internet users around the globe.',
+			color: colors.forth
+		}
+	];
 </script>
 
-<Timeline position="alternate">
-	<TimelineItem>
-		<TimelineOppositeContent slot="opposite-content">
-			<p class="oposite-content-title">1950s</p>
-		</TimelineOppositeContent>
+<Timeline
+	position="alternate"
+	style={`
+  // border: solid var(--border-color) 1px;
+  border-radius: 3%;
+  padding: 1rem;
+  `}
+>
+	{#each items as item}
+		<TimelineItem>
+			<TimelineOppositeContent slot="opposite-content">
+				<p class="oposite-content-title">{item.year}</p>
+			</TimelineOppositeContent>
 
-		<TimelineSeparator>
-			<TimelineDot style={'background-color: #7CD5E2; border-color: #fff;'} />
-			<TimelineConnector />
-		</TimelineSeparator>
-		<TimelineContent style={'height: 120px;'}>
-			<h2 class="content-title">The first ever WAN</h2>
-			<p class="content-description">The first known WAN was created by the U.S. Air Force.</p>
-		</TimelineContent>
-	</TimelineItem>
-
-	<TimelineItem>
-		<TimelineOppositeContent slot="opposite-content">
-			<p class="oposite-content-title">1969</p>
-		</TimelineOppositeContent>
-
-		<TimelineSeparator>
-			<TimelineDot style={'background-color: #FEC048; border-color: #fff;'} />
-			<TimelineConnector />
-		</TimelineSeparator>
-		<TimelineContent>
-			<h2 class="content-title">First online message</h2>
-			<p class="content-description">The first online message was sent.</p>
-		</TimelineContent>
-	</TimelineItem>
-
-	<TimelineItem>
-		<TimelineOppositeContent slot="opposite-content">
-			<p class="oposite-content-title">1971</p>
-		</TimelineOppositeContent>
-
-		<TimelineSeparator>
-			<TimelineDot style={'background-color: #DD84C1; border-color: #fff;'} />
-			<TimelineConnector />
-		</TimelineSeparator>
-		<TimelineContent style={'height: 120px;'}>
-			<h2 class="content-title">First Email</h2>
-			<p class="content-description">
-				The first network email was sent by computer engineer Ray Tomlinson.
-			</p>
-		</TimelineContent>
-	</TimelineItem>
-
-	<TimelineItem>
-		<TimelineOppositeContent slot="opposite-content">
-			<p class="oposite-content-title">1991</p>
-		</TimelineOppositeContent>
-
-		<TimelineSeparator>
-			<TimelineDot style={'background-color: #FB6958; border-color: #fff;'} />
-			<TimelineConnector />
-		</TimelineSeparator>
-		<TimelineContent>
-			<h2 class="content-title">First WebPage</h2>
-		</TimelineContent>
-	</TimelineItem>
-
-	<TimelineItem>
-		<TimelineOppositeContent slot="opposite-content">
-			<p class="oposite-content-title">1995</p>
-		</TimelineOppositeContent>
-
-		<TimelineSeparator>
-			<TimelineDot style={'background-color: #7DD5E0; border-color: #fff;'} />
-			<TimelineConnector />
-		</TimelineSeparator>
-		<TimelineContent>
-			<h2 class="content-title">Amazon Launch</h2>
-		</TimelineContent>
-	</TimelineItem>
-
-	<TimelineItem>
-		<TimelineOppositeContent slot="opposite-content">
-			<p class="oposite-content-title">1997</p>
-		</TimelineOppositeContent>
-
-		<TimelineSeparator>
-			<TimelineDot style={'background-color: #FEC048; border-color: #fff;'} />
-			<TimelineConnector />
-		</TimelineSeparator>
-		<TimelineContent>
-			<h2 class="content-title">Google Launch</h2>
-		</TimelineContent>
-	</TimelineItem>
-
-	<TimelineItem>
-		<TimelineOppositeContent slot="opposite-content">
-			<p class="oposite-content-title">2004</p>
-		</TimelineOppositeContent>
-
-		<TimelineSeparator>
-			<TimelineDot style={'background-color: #FB6958; border-color: #fff;'} />
-			<TimelineConnector />
-		</TimelineSeparator>
-		<TimelineContent>
-			<h2 class="content-title">Facebook Launch</h2>
-		</TimelineContent>
-	</TimelineItem>
+			<TimelineSeparator>
+				<TimelineDot style={`background-color: ${item.color}; border-color: #fff;`} />
+				<TimelineConnector />
+			</TimelineSeparator>
+			<TimelineContent>
+				<h3 class="content-title">{item.title}</h3>
+				<p class="content-description">{item.description}</p>
+			</TimelineContent>
+		</TimelineItem>
+	{/each}
 </Timeline>
 
 <style>
@@ -126,8 +106,10 @@
 	.content-description {
 		margin: 0;
 		padding: 0;
-		margin-top: 10px;
+		margin-top: 1rem;
 		color: #d1d3dd;
 		font-weight: lighter;
+		/* border: solid red 1px; */
+		padding: 1rem 0;
 	}
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,13 +3,32 @@
 	import Example from './example.svelte';
 </script>
 
-<div class="header">Svelte-vertical-timeline</div>
-<Example />
+<div class="header"><h1 class="header__title">svelte-vertical-timeline</h1></div>
+
+<div class="main">
+	<div class="example-container">
+		<Example />
+	</div>
+</div>
 
 <style>
 	.header {
 		height: 50px;
 		color: var(--main-color);
-		border-bottom: solid 1px var(--main-color);
+		/* border-bottom: solid 1px var(--main-color); */
+		text-align: center;
+	}
+	.header__title {
+		font-size: 1.4rem;
+	}
+	.main {
+		/* border: solid 1px #fff; */
+		margin-top: 1rem;
+		max-width: 768px;
+		margin: 0 auto;
+	}
+	.example-container {
+		/* border: solid red 1px; */
+		/* border-radius: 10%; */
 	}
 </style>
