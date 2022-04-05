f0e48571c6164386fa1c229628861b2457ad3ce2
Katsuki <Katsuki_Sato0130@icloud.com>
2022-04-05 09:13:30 +0900

    Export components

---
+++ src/lib/index.ts
@@ -0,0 +1,17 @@
+import Timeline from '$lib/components/Timeline.svelte';
+import TimelineItem from '$lib/components/TimelineItem.svelte';
+import TimelineSeparator from '$lib/components/TimelineSeparator.svelte';
+import TimelineDot from '$lib/components/TimelineDot.svelte';
+import TimelineConnector from '$lib/components/TimelineConnector.svelte';
+import TimelineContent from '$lib/components/TimelineContent.svelte';
+import TimelineOppositeContent from '$lib/components/TimelineOppositeContent.svelte';
+
+export {
+	Timeline,
+	TimelineItem,
+	TimelineSeparator,
+	TimelineDot,
+	TimelineConnector,
+	TimelineContent,
+	TimelineOppositeContent
+};
