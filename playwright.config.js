0d16fa5b070860bebf7f9084352a0f3b46d8bc1f
K-Sato1995 <Katsuki_Sato0130@icloud.com>
2024-05-12 16:43:35 +0900

    chore: update packages version

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
 	webServer: {
-		command: 'npm run build && npm run preview',
+		command: 'npm run build && npm run preview:app',
 		port: 3000
 	}
 };
